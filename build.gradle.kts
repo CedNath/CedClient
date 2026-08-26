a8c1ca9ef2bd9b4c3e35d750813732049d94c786
CedNath <nathan.bleuset@gmail.com>
2026-08-26 18:24:05 +0200

    idk what to type here

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT"
     id("maven-publish")
 }
+
 version = "${project.property("mod_version")}-${project.property("minecraft_version")}"
 group = project.property("maven_group") as String
 
